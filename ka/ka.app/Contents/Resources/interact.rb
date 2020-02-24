267ae338eff12600718189071edc0a1b20e59b51
TheDread-Pirate-Roberts <info@zachalewel.com>
2020-02-24 13:24:40 -0700

    fixes vs code indent weirdness

--- ka/ka.app/Contents/Resources/interact.rb
+++ ka/ka.app/Contents/Resources/interact.rb
@@ -3,10 +3,10 @@ sleep 3
 def interact
   %x(osascript -e 'tell application "System Events" to tell process "Microsoft Teams"
 	tell window 2
-		delay 1
-		key code 116
-		delay 1
-		key code 121
+	  delay 1
+	  key code 116
+	  delay 1
+	  key code 121
 	end tell
   end tell')
 end
