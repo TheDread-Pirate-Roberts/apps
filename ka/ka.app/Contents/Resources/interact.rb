74e08259b6af6bd42c9938f6f17d4cf15ddc0f8f
TheDread-Pirate-Roberts <info@zachalewel.com>
2020-02-24 13:19:09 -0700

    adds non zip version

---
+++ ka/ka.app/Contents/Resources/interact.rb
@@ -0,0 +1,18 @@
+sleep 3
+
+def interact
+  %x(osascript -e '
+tell application "System Events" to tell process "Microsoft Teams"
+	tell window 2
+		delay 1
+		key code 116
+		delay 1
+		key code 121
+	end tell
+end tell
+')
+end
+
+loop do
+  interact
+end
